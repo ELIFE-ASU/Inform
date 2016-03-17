3bc540754d198d228bce681c635907fec2cbc354
Douglas Moore <doug@dglmoore.com>
2016-03-17 10:39:22 -0700

    Improve tests of active_information

--- test/unittests/time_series.cpp
+++ test/unittests/time_series.cpp
@@ -27,20 +27,57 @@ TEST(ActiveInformation, SingleSeries_InvalidHistory)
 TEST(ActiveInformation, SingleSeries)
 {
     using namespace std;
-    auto const series = vector<bool>{1,1,0,0,1,0,0,1};
-    ASSERT_NEAR(0.918296, inform::active_information(begin(series), end(series), 2), 1e-6);
+    auto series = vector<bool>{1,1,0,0,1,0,0,1};
+    EXPECT_NEAR(0.918296, inform::active_information(begin(series), end(series), 2), 1e-6);
 
-    bool series_array[] = {1,1,0,0,1,0,0,1};
-    ASSERT_NEAR(0.918296, inform::active_information(begin(series_array), end(series_array), 2), 1e-6);
+    /// WORK THESE OUT BY HAND
+    series = {1,0,0,0,0,0,0,0,0};
+    EXPECT_NEAR(0.0, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {0,0,1,1,1,1,0,0,0};
+    EXPECT_NEAR(0.305958, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {1,0,0,0,0,0,0,1,1};
+    EXPECT_NEAR(0.347458, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {1,0,0,0,0,0,0,1,1};
+    EXPECT_NEAR(0.347458, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {0,0,0,0,0,1,1,0,0};
+    EXPECT_NEAR(0.399533, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {0,0,0,0,1,1,0,0,0};
+    EXPECT_NEAR(0.399533, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {1,1,1,0,0,0,0,1,1};
+    EXPECT_NEAR(0.305958, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {0,0,0,1,1,1,1,0,0};
+    EXPECT_NEAR(0.305958, inform::active_information(begin(series), end(series), 2), 1e-6);
+
+    series = {0,0,0,0,0,0,1,1,0};
+    EXPECT_NEAR(0.347458, inform::active_information(begin(series), end(series), 2), 1e-6);
 }
 
 TEST(ActiveInformation, EnsembleSeries)
 {
     using namespace std;
-    auto const series = vector<bool>{
+    auto series = vector<bool>{
         1,1,0,0,1,0,0,1,
         0,0,0,1,0,0,0,1,
     };
-
     ASSERT_NEAR(0.459148, inform::active_information(begin(series), end(series), 8, 2), 1e-6);
+
+    series = {
+        1,0,0,0,0,0,0,0,0,
+        0,0,1,1,1,1,0,0,0,
+        1,0,0,0,0,0,0,1,1,
+        1,0,0,0,0,0,0,1,1,
+        0,0,0,0,0,1,1,0,0,
+        0,0,0,0,1,1,0,0,0,
+        1,1,1,0,0,0,0,1,1,
+        0,0,0,1,1,1,1,0,0,
+        0,0,0,0,0,0,1,1,0,
+    };
+    ASSERT_NEAR(0.3080467, inform::active_information(begin(series), end(series), 9, 2), 1e-6);
 }
