547edb85292be5e6e19deabc6c092568c47c9201
Douglas Moore <doug@dglmoore.com>
2016-03-16 17:23:58 -0700

    Create an active_information overload averaging over initial states

--- include/inform/time_series.h
+++ include/inform/time_series.h
@@ -8,6 +8,8 @@
 #include <iterator>
 #include <type_traits>
 
+#include <algorithm>
+
 namespace inform
 {
     template <typename Iter>
@@ -24,7 +26,7 @@ namespace inform
         while (future != last)
         {
             auto const history = encode_state(first, future);
-            auto const state   = (history << 1) ^ (*future);
+            auto const state   = history ^ (*future << k);
 
             states.tic(state);
             histories.tic(history);
@@ -55,6 +57,38 @@ namespace inform
         auto futures   = distribution(2);
 
         active_information_distributions(first, last, k, states, histories, futures);
+
+        return mutual_information(states, histories, futures);
+    }
+
+    template <typename Iter>
+    auto active_information(Iter first, Iter last, size_t time_steps, uint64_t k) -> double
+    {
+        int64_t const size = std::distance(first, last);
+        if (size <= 1)
+        {
+            auto msg = "inform::active_information: time series must have more than one observation";
+            throw std::invalid_argument(msg);
+        }
+        else if (size - k <= 1)
+        {
+            auto msg = "inform::active_information: history length must be at least one less than the time series length";
+            throw std::invalid_argument(msg);
+        }
+        else if (size % time_steps != 0)
+        {
+            auto msg = "inform::active_information: each time series must have the same number of time steps";
+            throw std::invalid_argument(msg);
+        }
+
+        auto states    = distribution(2 << k);
+        auto histories = distribution(1 << k);
+        auto futures   = distribution(2);
+
+        for (auto tail = first + time_steps; first != last; first += time_steps, tail += time_steps)
+        {
+            active_information_distributions(first, tail, k, states, histories, futures);
+        }
 
         return mutual_information(states, histories, futures);
     }
--- test/unittests/time_series.cpp
+++ test/unittests/time_series.cpp
@@ -33,3 +33,14 @@ TEST(ActiveInformation, SingleSeries)
     bool series_array[] = {1,1,0,0,1,0,0,1};
     ASSERT_NEAR(0.918296, inform::active_information(begin(series_array), end(series_array), 2), 1e-6);
 }
+
+TEST(ActiveInformation, EnsembleSeries)
+{
+    using namespace std;
+    auto const series = vector<bool>{
+        1,1,0,0,1,0,0,1,
+        0,0,0,1,0,0,0,1,
+    };
+
+    ASSERT_NEAR(0.459148, inform::active_information(begin(series), end(series), 8, 2), 1e-6);
+}
